6c20da8cdf4df75553bd585ce2ff19b7938268bc
prabalsaxena <prabalsaxena4@gmail.com>
2024-11-17 11:29:17 +0530

    issue fixes and library add

--- frontend/src/app/layout/library/library.component.ts
+++ frontend/src/app/layout/library/library.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject, OnInit} from '@angular/core';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterModule} from '@angular/router';
+import {SmallSongCardComponent} from '../../shared/small-song-card/small-song-card.component';
+import {SongService} from '../../service/song.service';
+import {ReadSong} from '../../service/model/song.model';
 
 @Component({
   selector: 'app-library',
   standalone: true,
   imports: [
     FaIconComponent,
-    RouterLink
+    RouterModule, SmallSongCardComponent
   ],
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss'
 })
-export class LibraryComponent {
+export class LibraryComponent implements OnInit {
+
+  private songService = inject(SongService);
+
+  songs: Array<ReadSong> = [];
+
+  constructor() {
+    effect(() => {
+      if(this.songService.getAllSig().status==="OK") {
+        this.songs  = this.songService.getAllSig().value!;
+      }
+    })
+  }
+
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
 
 }
